ccb7926a762dd64411d97ae6c9e5f1d20cb6ff04
OursBlanc42 <regniersimon@hotmail.com>
2025-02-25 11:12:20 +0100

    pep8 compliance and add docstring

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -5,10 +5,14 @@ api = Namespace('users', description='User operations')
 
 # Define the user model for input validation and documentation
 user_model = api.model('User', {
-    'first_name': fields.String(required=True, description='First name of the user'),
-    'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'first_name': fields.String(required=True,
+                                description='First name of the user'),
+    'last_name': fields.String(required=True,
+                               description='Last name of the user'),
+    'email': fields.String(required=True,
+                           description='Email of the user')
 })
+
 
 @api.route('/')
 class UserList(Resource):
@@ -20,13 +24,18 @@ class UserList(Resource):
         """Register a new user"""
         user_data = api.payload
 
-        # Simulate email uniqueness check (to be replaced by real validation with persistence)
+        # Simulate email uniqueness check
+        # (to be replaced by real validation with persistence)
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
 
         new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+        return {
+            'id': new_user.id,
+            'first_name': new_user.first_name,
+            'last_name': new_user.last_name,
+            'email': new_user.email}, 201
 
     @api.response(200, 'List of users retrieved successfully')
     def get(self):
@@ -52,13 +61,26 @@ class UserResource(Resource):
         user = facade.get_user(user_id)
         if not user:
             return {'error': 'User not found'}, 404
-        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email}, 200
 
     @api.response(200, "User succeffuly updated")
     @api.response(404, "User not found")
     @api.response(400, "Invalid input data")
     def put(self, user_id):
-        """Update a user"""
+        """
+        Update user from user_id
+
+        Args:
+            user_id (UUID): UUID
+
+        Returns:
+            user: user data updated
+            None : if invalid input data
+        """
         user_data = api.payload
 
         if not user_data or not isinstance(user_data, dict):
--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 
+
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
