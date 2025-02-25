d32cca459511944b81186fd8b95a9e8e673e0e55
OursBlanc42 <regniersimon@hotmail.com>
2025-02-25 10:15:01 +0100

    implement user endpoint (create user, list user, update user)

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
@@ -9,10 +10,48 @@ class HBnBFacade:
 
     # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_all_users(self):
+        users = self.user_repo.get_all()
+        return users
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    def update_user(self, user_id, user_data):
+        """
+        update_user
+
+        Update user information base on user_id
+        1 - Check if the user exist
+        2 - Input are validated in 'put()' no need to recheck
+        3 - Loop through user data and update data
+        4 - Save the updated user in the repository
+
+        Args:
+            user_id (UUID): UUID
+            user_data (dict): Dictionnary of data
+
+        Returns:
+            user (User): instance of the user
+            None: if the user does not exist
+        """
+
+        user = self.user_repo.get(user_id)
+
+        if not user:
+            return None
+
+        if user:
+            for key, value in user_data.items():
+                setattr(user, key, value)
+            self.user_repo.update(user, user_data)
+        return user
