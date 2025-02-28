b6f0143fe9f22831847f502e4c7c1c96203743ca
OursBlanc42 <regniersimon@hotmail.com>
2025-02-28 17:18:20 +0100

    fix path specific /api/v1/places/<place_id>/reviews for listing review by place

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.reviews import api as reviews_ns, places_reviews_ns
 from app.api.v1.places import api as places_ns
 
 
@@ -16,5 +16,6 @@ def create_app():
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(places_reviews_ns, path='/api/v1/places')
 
     return app
