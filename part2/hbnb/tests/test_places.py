c88d859453c5035c262cfdce837cad4c20510c68
Ali <a.medjnoun@gmail.com>
2025-03-01 20:57:48 +0100

    test file to be tested

--- part2/hbnb/tests/test_places.py
+++ part2/hbnb/tests/test_places.py
@@ -30,7 +30,17 @@ class TestPlaceEndpoints(unittest.TestCase):
         self.assertEqual(place_response.status_code, 201)
         self.place_id = place_response.json['id']
 
-
+    def test_get_place(self):
+        """Test retrieving a specific place"""
+        response = self.client.get(f'/api/v1/places/{self.place_id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['id'], self.place_id)
+        self.assertEqual(response.json['title'], "Tattoine hut")
+        self.assertEqual(response.json['description'], "A place with 3 suns to stay")
+        self.assertEqual(response.json['price'], 100.42)
+        self.assertEqual(response.json['latitude'], 37.7749)
+        self.assertEqual(response.json['longitude'], -122.4194)
+        self.assertEqual(response.json['owner'], self.user_id)
 
     def test_create_place(self):
         """Test creating a new place with valid data"""
@@ -70,13 +80,6 @@ class TestPlaceEndpoints(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json, list)
 
-    def test_get_place(self):
-        """Test retrieving a specific place"""
-        response = self.client.get(f'/api/v1/places/{self.place_id}')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json['id'], self.place_id)
-        self.assertEqual(response.json['title'], "Tattoine hut")
-
     def test_get_place_not_found(self):
         """Test retrieving a place that does not exist"""
         response = self.client.get('/api/v1/places/non-existent-id')
@@ -86,12 +89,13 @@ class TestPlaceEndpoints(unittest.TestCase):
         """Test updating a place"""
         response = self.client.put(f'/api/v1/places/{self.place_id}', json={
             "title": "Dagobah cave",
-            "price": 13.37
+            "price": 13.37,
+            "description": "A place to face your fears"
         })
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['title'], "Dagobah cave")
         self.assertEqual(response.json['price'], 13.37)
-        self.assertEqual(response.json['description'], "A place with 3 suns to stay")
+        self.assertEqual(response.json['description'], "A place to face your fears")
 
     def test_update_place_not_found(self):
         """Test updating a place that does not exist"""
@@ -111,7 +115,7 @@ class TestPlaceEndpoints(unittest.TestCase):
 Because Ewoks should not exist
 
     def test_delete_place(self):
-        """Test deleting a place"""
+        Test deleting a place
         temp_place = self.client.post('/api/v1/places/', json={
             "title": "Endor forest",
             "description": "Home of the Ewoks, must be deleted",
@@ -129,7 +133,7 @@ Because Ewoks should not exist
         self.assertEqual(get_response.status_code, 404)
 
     def test_delete_place_not_found(self):
-        """Test deleting a place that does not exist"""
+        Test deleting a place that does not exist
         response = self.client.delete('/api/v1/places/non-existent-id')
         self.assertEqual(response.status_code, 404)
 """
